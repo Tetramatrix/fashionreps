5b98907a997814d29f6a0e4a56af03c2a37cd5c4
Chi <chibox@gmail.com>
2019-09-23 16:13:56 +0200

    Initial

--- docs/masonry.js
+++ docs/masonry.js
@@ -1,7 +1,7 @@
 /***************************************************************
  *  Copyright notice
  *
- *  (c) 2011-2019 Chi Hoang <info@chihoang.de>
+ *  (c) 2011-2019 Chi Hoang <rebrobates@gmail.com>
  *  All rights reserved
  *
  ***************************************************************/
@@ -13,7 +13,8 @@
   var brick_stack = [];
   var url_stack = [];
   var url_back = [];
-  var root_button = "Home"
+  var root_but = "Home";
+  var about_but = "About Us";
     
   function gridview ()
   {
@@ -43,7 +44,7 @@
         "position": "absolute",
         "top": "-600px",
         "height": "600px",
-        "left" : "140px"
+        "left" : "0px"
       })
       $j.getJSON(url, function(json)
       {    
@@ -55,7 +56,7 @@
           })).animate({
             "opacity": "100.0",
             "top": "0px",
-            "left": "140px"
+            "left": "0px"
           }, 900, function() {
             // Unset it here, this lets the button be clickable again
             btn.data('running', false);
@@ -79,7 +80,7 @@
             "opacity": "0.0"
           }).animate({
               "opacity": "100.0",
-              "left" : "140px",
+              "left" : "0px",
               "top": "10px"
           }, 900, function()
             {
@@ -114,42 +115,45 @@
     } else
     {
       url_stack.push(url);  
+      console.log(url_stack);
     }
     c.css({
       "z-index" : "-35",
       "position": "absolute",
-      "left" : "140px",
-      "top" : "0px"
+      "left" : "0px",
+      "top" : "10px"
     }).animate({
       "position": "relative",
       "opacity": "0.0",
       "top": "+=600px",
-      "left" : "140px"
+      "left" : "0px"
     }, 900, function()
       {
       
       //$('#singleview').load("https://cors-anywhere.herokuapp.com/"+url);
       // Animation complete.
-
+      /*
       $j.ajax({url: "https://cors-anywhere.herokuapp.com/"+url,
       				type: 'GET',
       				dataType: 'html',
       			 success: function(json)
       {
+      */
+      
+      $j.getJSON(url,function(json) {      
         if (json)
         {
-        	 s.html(json);
-        	 
-        	/*
+        	 /* s.html(json); */
+
           s.css({
             "position": "absolute",
             "top": "-600px",
             "height": "600px",
-            "left" : "140px"
+            "left" : "170px"
           }).animate({
             "opacity": "100.0",
             "top": "10px",
-            "left" : "140px"
+            "left" : "170px"
           }, 900, function() {
             // Unset it here, this lets the button be clickable again
             btn.data('running', false);
@@ -157,131 +161,149 @@
             "visibility": "visible",
             "position": "relative"
           }));
-          */  
         }        
-      }});
+      });
             
     });
   }
   
-  function ajax () {
+  function ajax (self) {
     // Menu ajax script
     $j("#tx-charbeitsbeispiele-pi1 #menu li").click(function()
     {
-      $j.getJSON($j(this).find('a').attr('href'), function(json)
-      {
-        if (json && json.length)
-        {
-          var container = $j('#tx-charbeitsbeispiele-pi1 #container');
-          container.masonry();
-          var toggle = this.url.match(/toggle=on/g);
-          var clrscreen = this.url.match(/screen=clear/g);
-          
-          if (clrscreen) {
-          	container.empty();
-          }
-          
-          $j.each(json.reverse(), function(idx, ele)
-          {
-            //if (ele.toggle == "on")
-            if (toggle)
-            {
-              if (ele.Additem == "Append" && ele.Isimage == "true" )
-              {
-                container.append($j("#brickTemplate").tmpl(ele).css({
-                "display": "block"
-                })).masonry('reload');  
-              } else if (ele.Additem == "Append")
-              {
-                container.append($j("#defaultTemplate").tmpl(ele).css({
-                "display": "block"
-                })).masonry('reload');
-              } else if (ele.Additem == "Prepend" && ele.Isimage == "true" )
-              {
-                container.prepend($j("#brickTemplate").tmpl(ele).css({
-                  "display": "block"
-                })).masonry('reload');  
-              } else
-              {
-                container.prepend($j("#defaultTemplate").tmpl(ele).css({
-                  "display": "block"
-                })).masonry('reload');
-              }
-            } else
-            {
-              $j('.brick').remove(":contains('" + ele.Headline + "')");
-              container.masonry('reload');
-            }
-            container.imagesLoaded()
-            .progress( function( instance, image ) {
-    					var result = image.isLoaded ? 'loaded' : 'broken';
-    					console.log( 'image is ' + result + ' for ' + image.img.src );
-  					})
-            .done(function()
-            {
-              // bricks correct height
-              $j("#tx-charbeitsbeispiele-pi1 #container .brick").each(function()
-              {
-                var height = $j(this).find("img").attr("height");
-                if ( height == undefined )
-                {
-                  $j(this).find(">div").css({
-                    height: "300px"
-                  }); 
-                } else {
-                  $j(this).find(">div").css({
-                    height: height
-                  }); 
-                }
-                $j(this).mousemove(function()
-                {
-                  var content = $j(this).find(">div");
-                  var summary = $j(this).find(".teaser");
-                  var height = $j(this).find("img").attr("height");
-                  if ( height == undefined )
-                  {
-                    content.css({
-                      height: "300px"
-                    }); 
-                  } else {
-                    content.css({
-                      height: height
-                    }); 
-                   }
-                  if (!content.is(":animated") && summary.is(":not(:visible)"))
-                  {
-                    content.css({
-                      height: height,
-                      position: "relative",
-                      top: -35 - summary.height()
-                    });
-                    summary.show();
-                    brick_stack.unshift(this);
-                    content.animate({
-                      top: 0
-                    });
-                    while (brick_stack.length > 1)
-                    {
-                      hide_summary(brick_stack.pop());
-                    }
-                  }
-                });
-                $j(this).mouseleave(function() {
-                  hide_summary(brick_stack.pop());
-                });
-              });
-            });
-          });
-        } else
-        {
-          return false; // don't follow the link!
-        }
-      });
+    	var n = $j(this).find('a').text();
+    	console.log(n);    	
+    	if (n == about_but || self.url_stack().length>0) {
+    		
+				 console.log(self.url_stack());
+				 if (self.url_stack().length==0) 
+				 {
+				 		var toggle = $("#tx-charbeitsbeispiele-pi1 #menu li")[1];
+						$(toggle).removeClass('ref_no').addClass('ref_act');
+						var theHref = $(toggle).find('a').attr("href").replace(/\?toggle=.*/g, '');		
+						$(toggle).find('a').attr("href", theHref + "?toggle=on");	
+				 		singleview($j(this).find('a').attr('href'));
+				 } else {
+						bbut (); 	
+				 }
+    		return false;
+    		
+    	} else {
+    			$j.getJSON($j(this).find('a').attr('href'), function(json)
+	      {
+	        if (json && json.length)
+	        {
+	          var container = $j('#tx-charbeitsbeispiele-pi1 #container');
+	          container.masonry();
+	          var toggle = this.url.match(/toggle=on/g);
+	          var clrscreen = this.url.match(/screen=clear/g);
+	          
+	          if (clrscreen) {
+	          	container.empty();
+	          }
+	          
+	          $j.each(json.reverse(), function(idx, ele)
+	          {
+	            //if (ele.toggle == "on")
+	            if (toggle)
+	            {
+	              if (ele.Additem == "Append" && ele.Isimage == "true" )
+	              {
+	                container.append($j("#brickTemplate").tmpl(ele).css({
+	                "display": "block"
+	                })).masonry('reload');  
+	              } else if (ele.Additem == "Append")
+	              {
+	                container.append($j("#defaultTemplate").tmpl(ele).css({
+	                "display": "block"
+	                })).masonry('reload');
+	              } else if (ele.Additem == "Prepend" && ele.Isimage == "true" )
+	              {
+	                container.prepend($j("#brickTemplate").tmpl(ele).css({
+	                  "display": "block"
+	                })).masonry('reload');  
+	              } else
+	              {
+	                container.prepend($j("#defaultTemplate").tmpl(ele).css({
+	                  "display": "block"
+	                })).masonry('reload');
+	              }
+	            } else
+	            {
+	              $j('.brick').remove(":contains('" + ele.Headline + "')");
+	              container.masonry('reload');
+	            }
+	            container.imagesLoaded()
+	            .progress( function( instance, image ) {
+	    					var result = image.isLoaded ? 'loaded' : 'broken';
+	    					console.log( 'image is ' + result + ' for ' + image.img.src );
+	  					})
+	            .done(function()
+	            {
+	              // bricks correct height
+	              $j("#tx-charbeitsbeispiele-pi1 #container .brick").each(function()
+	              {
+	                var height = $j(this).find("img").attr("height");
+	                if ( height == undefined )
+	                {
+	                  $j(this).find(">div").css({
+	                    height: "300px"
+	                  }); 
+	                } else {
+	                  $j(this).find(">div").css({
+	                    height: height
+	                  }); 
+	                }
+	                $j(this).mousemove(function()
+	                {
+	                  var content = $j(this).find(">div");
+	                  var summary = $j(this).find(".teaser");
+	                  var height = $j(this).find("img").attr("height");
+	                  if ( height == undefined )
+	                  {
+	                    content.css({
+	                      height: "300px"
+	                    }); 
+	                  } else {
+	                    content.css({
+	                      height: height
+	                    }); 
+	                   }
+	                  if (!content.is(":animated") && summary.is(":not(:visible)"))
+	                  {
+	                    content.css({
+	                      height: height,
+	                      position: "relative",
+	                      top: -35 - summary.height()
+	                    });
+	                    summary.show();
+	                    brick_stack.unshift(this);
+	                    content.animate({
+	                      top: 0
+	                    });
+	                    while (brick_stack.length > 1)
+	                    {
+	                      hide_summary(brick_stack.pop());
+	                    }
+	                  }
+	                });
+	                $j(this).mouseleave(function() {
+	                  hide_summary(brick_stack.pop());
+	                });
+	              });
+	            });
+	          });
+	        } else
+	        {
+	          return false; // don't follow the link!
+	        }
+	      });
       return false; // don't follow the link!
+    	}
     });
   }
   
-  function reload ()
+  function reload (self)
   {
     $j(window).unload(function()
     {
@@ -325,25 +347,27 @@
     // Menu toggle script
     menu.toggle(
       function() {
-        var n = $j(this).find('a').text();
-        // Klick "Alle"-Button
-        if (n == root_button) {
-        	var container = $j('#tx-charbeitsbeispiele-pi1 #container');
-        	container.empty();
-          $j.each(menu, function(idx, ele)
-          {
-          // Alle anderen buttons off
-          if ($j(ele).find('a').text() != root_button) {
-            $j(ele).removeClass('ref_act').addClass('ref_no');
-            var theHref = $j(ele).find('a').attr("href").replace(/\?toggle=.*/g, '');
-            $j(ele).find('a').attr("href", theHref + "?toggle=off");
-          } else {
-            // Alle buttons on
-            $j(ele).removeClass('ref_no').addClass('ref_act');
-            var theHref = $j(ele).find('a').attr("href").replace(/\?toggle=.*/g, '');
-            $j(ele).find('a').attr("href", theHref + "?toggle=on");
-          }
-        });
+      	if (self.url_stack().length==0) {
+      		var n = $j(this).find('a').text();
+	        // Klick "Alle"-Button
+	        if (n == root_but) {
+	        	var container = $j('#tx-charbeitsbeispiele-pi1 #container');
+	        	container.empty();
+	          $j.each(menu, function(idx, ele)
+	          {
+	          // Alle anderen buttons off
+	          if ($j(ele).find('a').text() != root_but) {
+	            $j(ele).removeClass('ref_act').addClass('ref_no');
+	            var theHref = $j(ele).find('a').attr("href").replace(/\?toggle=.*/g, '');
+	            $j(ele).find('a').attr("href", theHref + "?toggle=off");
+	          } else {
+	            // Alle buttons on
+	            $j(ele).removeClass('ref_no').addClass('ref_act');
+	            var theHref = $j(ele).find('a').attr("href").replace(/\?toggle=.*/g, '');
+	            $j(ele).find('a').attr("href", theHref + "?toggle=on");
+	          }
+	        });
+      	}
       }
     
       var params = "?toggle=on";
@@ -355,7 +379,7 @@
           var theHref = $j(ele).find('a').attr("href").replace(/\?toggle=.*/g, '');
           $j(ele).find('a').attr("href", theHref + params);
     
-        } else if ($j(ele).find('a').text() == root_button && $j(ele).hasClass('ref_act')) {
+        } else if ($j(ele).find('a').text() == root_but && n != about_but && $j(ele).hasClass('ref_act')) {
           params += "&screen=clear";
           $j(ele).removeClass('ref_act').addClass('ref_no');
           var theHref = $j(ele).find('a').attr("href").replace(/\?toggle=.*/g, '');
@@ -367,7 +391,7 @@
     
     function() {
       var n = $j(this).find('a').text();
-      if (n != root_button) {
+      if (n != root_but) {
         var params = "?toggle=off";
         $j.each(menu, function(idx, ele) {
           if ($j(ele).find('a').text() == n) {
@@ -382,7 +406,7 @@
             }
           }
           // Alle-Button Active
-          if ($j(ele).hasClass('ref_act') && $j(ele).find('a').text() == root_button) {
+          if ($j(ele).hasClass('ref_act') && $j(ele).find('a').text() == root_but) {
             // Clear Screen
             params += "&screen=clear";
             $j(ele).removeClass('ref_act').addClass('ref_no');
@@ -391,14 +415,14 @@
     
           }
         });
-      } else if (n == root_button) {
+      } else if (n == root_but) {
         $j.each(menu, function(idx, ele) {
-          if ($j(ele).find('a').text() == root_button) {
+          if ($j(ele).find('a').text() == root_but) {
             $j(this).removeClass('ref_no').addClass('ref_act');
             var theHref = $j(this).find('a').attr("href").replace(/\?toggle=.*/g, '');
             $j(this).find('a').attr("href", theHref + "?toggle=on");
     
-          } else if ($j(ele).find('a').text() != root_button) {
+          } else if ($j(ele).find('a').text() != root_but) {
             $j(ele).removeClass('ref_act').addClass('ref_no');
             var theHref = $j(ele).find('a').attr("href").replace(/\?toggle=.*/g, '');
             $j(ele).find('a').attr("href", theHref + "?toggle=off");
@@ -517,6 +541,15 @@
     });
   }
 
+  function bbut () {  	
+  	var toggle = $("#tx-charbeitsbeispiele-pi1 #menu li")[1];
+		//toggle.attr("unchecked", !toggle.attr("unchecked")).button("refresh");
+		$(toggle).removeClass('ref_act').addClass('ref_no');
+		var theHref = $(toggle).find('a').attr("href").replace(/\?toggle=.*/g, '');		
+		$(toggle).find('a').attr("href", theHref + "?toggle=off");
+  	maxmedia.gridview();
+  }
+  
   // Privat function
   function hide_summary(ele)
   {
@@ -543,16 +576,26 @@
     return gridview ( );
   }
   
-  Arbeitsbeispiele.prototype.ajax = function ()
+  Arbeitsbeispiele.prototype.ajax = function (self)
   {
-    return ajax();
+    return ajax(self);
   }
   
-  Arbeitsbeispiele.prototype.reload = function ()
+  Arbeitsbeispiele.prototype.reload = function (self)
   {
-    return reload();
+    return reload(self);
   }
 
+  Arbeitsbeispiele.prototype.bbut = function ()
+  {
+    return bbut();
+  }
+
+  Arbeitsbeispiele.prototype.url_stack = function ()
+  {
+    return url_stack;
+  }
+    
   function Arbeitsbeispiele (anonymous)
   {
     $j = anonymous;
@@ -575,19 +618,30 @@ var $ = jQuery.noConflict();
 $(document).ready(function() {
   $("#container").draggable({zIndex:-35});
   maxmedia = new Arbeitsbeispiele($);
-  maxmedia.reload();
-  maxmedia.ajax();
+  maxmedia.reload(maxmedia);
+  maxmedia.ajax(maxmedia);
   
   const nav = document.querySelector('#navigation');
 	const navTop = nav.offsetTop;
 
+   /*
 	function stickyNavigation() {
   	console.log('navTop = ' + navTop);
   	console.log('scrollY = ' + window.scrollY);
 	}
-
+		
 	window.addEventListener('scroll', stickyNavigation);
 	
+	*/
+	
+	/*
+	$("#bbut").click(function() {
+			var toggle = $("#tx-charbeitsbeispiele-pi1 #menu li")[1].trigger("click");
+			//$toggle.attr("checked", !$toggle.attr("checked")).button("refresh");
+			toggle.trigger("click");
+			maxmedia.gridview();
+	});
+	*/
 	$('.lazy').Lazy({
         // your configuration goes here
         scrollDirection: 'vertical',
